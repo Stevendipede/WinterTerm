ec68b12fc53a95744350da0c0f642229c6ee20bc
Stevendipede <dipedes@shernet.sheridancollege.ca>
2026-01-22 13:34:19 -0500

    added part time student class

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author iamst
+ */
+public class PartTimeStudent {
+    
+    private String studentNumber;
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+    
+    
+}
